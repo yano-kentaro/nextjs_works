9a3bcd6d81c330a9fbb89fb438a0114589cb8c84
ken <ynkn0829@gmail.com>
2022-11-03 21:25:50 +0900

    微修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,26 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+// Text
+import t from '@/text/text';
+
+// Get SaaSke Works Data from API
+async function getData() {
+  // 後々、APIからデータを取得するようにする
+  // 一旦、mockからデータを取得して実装する
+  return await import('@/mock/apps.json');
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const worksData = await getData();
   return (
+    <html lang="ja">
+      <head>
         <title>{t.appTitle}</title>
       </head>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
