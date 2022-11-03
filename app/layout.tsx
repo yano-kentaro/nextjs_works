8b9f8923a3b774f65339634aab4ec4666d0c2931
ken <ynkn0829@gmail.com>
2022-11-03 17:35:54 +0900

    MUI用のフォント導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+// MUI Fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
