920c36057b8211446b21584a6e18aea04b129e10
ken <ynkn0829@gmail.com>
2022-11-03 22:31:21 +0900

    型定義ファイル更新

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,1 +1,80 @@
-type uuid = string;
+type apiResGetApps = {
+  count: string;
+  list: {
+    appKey: string;
+    appName: string;
+    appShortName: string;
+    appStatus: 'active';
+    appGroup?: string;
+    appAuthor: {
+      name: string;
+      code: string;
+    };
+    appMemo?: string;
+    appColor: string;
+    appIcon: string;
+    appQrcode: string;
+    appFavorite?: string;
+    appUrl: string;
+  };
+};
+
+type apiResGetRecords = {
+  countTotal: string;
+  count: string;
+  fields: {
+    fieldCode?: string;
+    fieldKey: string;
+    fieldName: string;
+    fieldType: string;
+    fieldKind: string;
+  }[];
+  records: {
+    recordId: string;
+    recordTitle: string;
+    registAccount: {
+      name: string;
+      code: string;
+    };
+    registDate: string;
+    updateAccount: {
+      name: string;
+      code: string;
+    };
+    updateDate: string;
+    [key: string]: string;
+  }[];
+};
+
+type worksData = {
+  appKey: string;
+  appName: string;
+  appShortName: string;
+  appGroup: string;
+  appMemo: string;
+  appColor: string;
+  appIcon: string;
+  count: string;
+  fields: {
+    fieldCode?: string;
+    fieldKey: string;
+    fieldName: string;
+    fieldType: string;
+    fieldKind: string;
+  }[];
+  records: {
+    recordId: string;
+    recordTitle: string;
+    registAccount: {
+      name: string;
+      code: string;
+    };
+    registDate: string;
+    updateAccount: {
+      name: string;
+      code: string;
+    };
+    updateDate: string;
+    [key: string]: string;
+  }[];
+}[];
